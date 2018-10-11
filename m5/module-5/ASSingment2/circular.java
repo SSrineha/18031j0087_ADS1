9ceaa43d189f64dfd76bd5bb1b213e6e935a08b4
SSrineha <nehareddysree@gmail.com>
2018-10-11 14:36:46 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

---
+++ m5/module-5/ASSingment2/circular.java
@@ -0,0 +1,33 @@
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class circular {
+
+	public static void main(String[] args) 
+	{
+		LinkedList<Integer> ll=new LinkedList<>();
+		Scanner sc=new Scanner(System.in);
+		int ni=sc.nextInt();
+		for(int i=0;i<ni;i++)
+		{
+			int n=sc.nextInt();
+			int m=sc.nextInt();
+			for(int j=0;j<n;j++)
+			{
+				ll.add(j);
+			}
+			int temp=0;
+			while(ll.size()!=0)
+			{
+				temp=(temp+m-1)%ll.size();
+				System.out.print(ll.get(temp));
+				ll.remove(temp);
+				
+			}
+                           System.out.println("");
+		}
+
+	}
+
+}
