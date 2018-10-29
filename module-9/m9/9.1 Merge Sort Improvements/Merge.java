c2b2fc32d908e4044c866c7a0fbe6a42d21c50d0
SSrineha <nehareddysree@gmail.com>
2018-10-29 16:07:43 +0530

    modules

--- module-9/m9/9.1 Merge Sort Improvements/Merge.java
+++ module-9/m9/9.1 Merge Sort Improvements/Merge.java
@@ -1,12 +1,16 @@
 
+
+import java.util.Scanner;
+
+/* Java program for Merge Sort */
 public class Merge
 { 
-    void merge(int arr[], int l, int m, int r) 
+    void merge(String arr[], int l, int m, int r) 
     {  
-        int n1 = m - l + 1; 
+        int n1 = m-l+1; 
         int n2 = r - m; 
-        int L[] = new int [n1]; 
-        int R[] = new int [n2]; 
+        String L[] = new String [n1]; 
+        String R[] = new String [n2]; 
         for (int i=0; i<n1; ++i) 
             L[i] = arr[l + i]; 
         for (int j=0; j<n2; ++j) 
@@ -17,7 +21,8 @@ public class Merge
         int k = l; 
         while (i < n1 && j < n2) 
         { 
-            if (L[i] <= R[j]) 
+        	int s=L[i].compareTo(R[j]);
+            if (s<0) 
             { 
                 arr[k] = L[i]; 
                 i++; 
@@ -46,13 +51,15 @@ public class Merge
             k++; 
         } 
     } 
-    void sort(int arr[], int l, int r) 
+    void sort(String arr[], int l, int r) 
     { 
         if(r-l<=7) {
-        	for (int i = l+1; i < r; i++) {
+        	System.out.println("Insertion sort method invoked...");
+        	for (int i = l+1; i <= r; i++) {
 				for (int j = i; j > 0; j--) {
-					if(arr[j]<arr[j-1]){
-						int temp=arr[j];
+					int s=arr[j-1].compareTo(arr[j]);
+					if(s>0){
+						String temp=arr[j];
 						arr[j]=arr[j-1];
 						arr[j-1]=temp;
 					}
@@ -66,23 +73,26 @@ public class Merge
             merge(arr, l, m, r); 
         } 
     } 
-    static void printArray(int arr[]) 
+    static void printArray(String st[]) 
     { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i] + " "); 
+        String s1="["+st[0];
+        for (int i=1; i<st.length; i++) 
+        	 s1=s1+","+st[i];
+            System.out.print(s1+"]"); 
         System.out.println(); 
     } 
-  
+
     // Driver method 
     public static void main(String args[]) 
     { 
-        int arr[] = {12, 11, 13, 5, 6, 7,2,51}; 
-        Merge m=new Merge();
-        System.out.println("Given Array"); 
-        printArray(arr); 
-        m.sort(arr, 0, arr.length-1); 
-        System.out.println("\nSorted array"); 
-        printArray(arr); 
+    	Scanner scn=new Scanner(System.in);
+    	do {
+    		String st = scn.nextLine();
+			String sp[] = st.split(",");
+    		Merge m=new Merge();
+            m.sort(sp, 0, sp.length-1); 
+            printArray(sp); 
+    	}while(scn.hasNext());
+    
     } 
 } 
